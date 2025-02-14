37d9036e57edf140d8cf319f00786a935d9623ea
JBGruber <johannesb.gruber@gmail.com>
2025-02-14 12:10:36 +0100

    rename custom algo

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -3,12 +3,12 @@ import {
   QueryParams,
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
-import * as whatsAlf from './feed-1'
+import * as feedNL1 from './feed-nl-1'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
-  [whatsAlf.shortname]: whatsAlf.handler,
+  [feedNL1.shortname]: feedNL1.handler,
 }
 
 export default algos
