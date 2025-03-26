c1a703e7000368a0a7c4a73e42e7a828e626deb9
JBGruber <JohannesB.Gruber@gmail.com>
2025-03-26 10:31:50 +0000

    fix feed location

--- src/methods/feed-generation.ts
+++ src/methods/feed-generation.ts
@@ -11,7 +11,8 @@ export default function (server: Server, ctx: AppContext) {
     const feedUri = new AtUri(params.feed)
     const algo = algos[feedUri.rkey]
     if (
-      feedUri.hostname !== ctx.cfg.publisherDid ||
+      // turned off publisherDid validation
+      // feedUri.hostname !== ctx.cfg.publisherDid ||
       feedUri.collection !== 'app.bsky.feed.generator' ||
       !algo
     ) {
